a151a2d34e0b282a1314d9e5fde6806e2ffb2c92
Merve TOPAL <merve.tpl@windowslve.com>
2022-01-08 19:55:24 +0300

    *[development] *[Dbcontext class projeye eklenmiştir.]

---
+++ MT.MicroService.Data/FileContext/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MT.MicroService.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.MicroService.Data.FileContext
+{
+    class AppDbContext :DbContext
+    {
+        public AppDbContext( DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person>  Persons { get; set; }
+
+        public DbSet<ContactInfo> ContactInfos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
