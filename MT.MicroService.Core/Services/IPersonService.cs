8717e9dcf0ad4ba22601350e509747e786a4d1b2
Merve TOPAL <merve.tpl@windowslve.com>
2022-01-08 18:05:47 +0300

    *[development] *[Service katman için sınıflar eklenmiştir.]

---
+++ MT.MicroService.Core/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using MT.MicroService.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.MicroService.Core.Services
+{
+   public interface IPersonService :IServices<Person>
+    {
+        Task<Person> GetWithContactInfoByPersonIdAsync(int personId);
+    }
+}
